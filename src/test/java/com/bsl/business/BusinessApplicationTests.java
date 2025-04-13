3d5afd910fd65518c096a616af0f916b6f5560c0
vinayakkshirsagar <kshirsagarvinayak97@gmail.com>
2025-04-13 20:21:34 +0530

    “some”

---
+++ src/test/java/com/bsl/business/BusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsl.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
